4842b04adf4de0b6ff584b8c6f9eb4d253b236e1
etaswi <emretaswi@outlook.de>
2015-07-07 20:41:04 +0200

    Update html_beispiel.java
    Geändert in Github-Web

--- html_beispiel.java
+++ html_beispiel.java
@@ -4,5 +4,7 @@ public class MeineGitKlasse {
 	public static void main(String[] args){
 		System.out.println("Dies ist ein Test");
 		System.out.println("Dies ist noch ein Test");
+		System.out.println("Noch einer");
+		System.out.println("Und noch einmal!!!");
 	}
 }
